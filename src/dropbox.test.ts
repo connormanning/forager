073969fbac4732a94842068a3386930a608529b2
Connor Manning <connor@hobu.co>
2021-01-21 14:37:43 -0600

    Add full dropbox support: read/write and streaming ops.  With tests.

--- src/dropbox.test.ts
+++ src/dropbox.test.ts
@@ -1,10 +1,16 @@
+import { Readable } from 'stream'
+
+import * as Util from './util'
+
 import { Dropbox } from '.'
 
+afterEach(jest.clearAllMocks)
+
 const filesListFolder = jest.fn()
+const filesDownload = jest.fn()
+const filesUpload = jest.fn()
 jest.mock('dropbox', () => ({
-  Dropbox: jest.fn(() => ({
-    filesListFolder,
-  })),
+  Dropbox: jest.fn(() => ({ filesListFolder, filesDownload, filesUpload })),
 }))
 
 const cred = { token: 'asdf' }
@@ -60,6 +66,81 @@ test('list: failure', async () => {
   const mock = filesListFolder.mockImplementationOnce(() => {
     throw new Error('asdf')
   })
-  expect(Dropbox.create(cred).list('asdf/fdsa')).rejects.toThrow()
+  await expect(Dropbox.create(cred).list('asdf/fdsa')).rejects.toThrow()
   expect(mock).toHaveBeenCalled()
+})
+
+test('read: failure', async () => {
+  const mock = filesDownload.mockImplementationOnce(() => {
+    throw new Error('asdf')
+  })
+  await expect(Dropbox.create(cred).read('asdf')).rejects.toThrow('asdf')
+  expect(mock).toHaveBeenCalled()
+})
+
+test('read: missing binary data', async () => {
+  const mock = filesDownload.mockImplementationOnce(() => ({ result: {} }))
+  await expect(Dropbox.create(cred).read('asdf')).rejects.toThrow(
+    /missing file contents/i
+  )
+  expect(mock).toHaveBeenCalled()
+})
+
+test('read: success', async () => {
+  const filename = 'a.txt'
+  const mock = filesDownload.mockImplementationOnce(() => ({
+    result: {
+      fileBinary: Buffer.from('asdf'),
+    },
+  }))
+  expect(await Dropbox.create(cred).read(filename)).toEqual(Buffer.from('asdf'))
+  expect(mock).toHaveBeenCalledWith({ path: `/${filename}` })
+})
+
+test('read: stream', async () => {
+  const filename = 'a.txt'
+  const mock = filesDownload.mockImplementationOnce(() => ({
+    result: {
+      fileBinary: Buffer.from('asdf'),
+    },
+  }))
+
+  const d = Dropbox.create(cred)
+  const stream = await d.createReadStream(filename)
+  expect(mock).toHaveBeenCalledWith({ path: `/${filename}` })
+  const data = (await Util.drain(stream)).toString()
+  expect(data).toEqual('asdf')
+})
+
+test('write: failure', async () => {
+  const path = 'a.txt'
+  const data = Buffer.from('asdf')
+  const mock = filesUpload.mockImplementationOnce(async () => {
+    throw new Error('fdsa')
+  })
+
+  const d = Dropbox.create(cred)
+  await expect(d.write(path, data)).rejects.toThrow('fdsa')
+  expect(mock).toHaveBeenCalledWith({ path: `/${path}`, contents: data })
+})
+
+test('write: success', async () => {
+  const path = 'a.txt'
+  const data = Buffer.from('asdf')
+  const mock = filesUpload.mockImplementationOnce(async () => {})
+
+  const d = Dropbox.create(cred)
+  await d.write(path, data)
+  expect(mock).toHaveBeenCalledWith({ path: `/${path}`, contents: data })
+})
+
+test('write: stream', async () => {
+  const path = 'a.txt'
+  const data = Buffer.from('asdf')
+  const stream = Readable.from(data)
+  const mock = filesUpload.mockImplementationOnce(async () => {})
+
+  const d = Dropbox.create(cred)
+  await d.writeStream(path, stream)
+  expect(mock).toHaveBeenCalledWith({ path: `/${path}`, contents: data })
 })
--- src/dropbox.ts
+++ src/dropbox.ts
@@ -1,7 +1,9 @@
+import { Readable } from 'stream'
 import D, { Dropbox } from 'dropbox'
 import fetch from 'node-fetch'
 
 import * as Types from './types'
+import * as Util from './util'
 
 export type Options = { token: string }
 export function isOptions(o: any): o is Options {
@@ -32,14 +34,48 @@ function isFile(m: D.files.Metadata): m is D.files.FileMetadataReference {
   return (m as any)['.tag'] === 'file'
 }
 
-export function create({ token: accessToken }: Options): Types.Listable {
+// For some reason this isn't included in the type definition.
+type FileMetadataWithBuffer = D.files.FileMetadata & { fileBinary?: Buffer }
+
+export function create({
+  token: accessToken,
+}: Options): Types.Listable &
+  Types.Readable &
+  Types.Writable &
+  Types.StreamReadable &
+  Types.StreamWritable {
   const dbx = new Dropbox({ accessToken, fetch })
+
+  async function read(path: string): Promise<Buffer> {
+    const response = await dbx.filesDownload({
+      path: coercePath(path),
+    })
+    const result: FileMetadataWithBuffer = response.result
+    if (!result.fileBinary) {
+      throw new Error('Missing file contents in Dropbox response')
+    }
+    return result.fileBinary
+  }
+
+  async function write(path: string, contents: Buffer | string) {
+    await dbx.filesUpload({ path: coercePath(path), contents })
+  }
+
+  // This API doesn't actually support streaming, so we'll just pass through to
+  // the normal read/write for compatibility.
+  async function createReadStream(path: string) {
+    const buffer = await read(path)
+    return Readable.from(buffer)
+  }
+
+  async function writeStream(path: string, stream: NodeJS.ReadableStream) {
+    const data = await Util.drain(stream)
+    return write(path, data)
+  }
 
   async function list(path: string): Promise<Types.List> {
     try {
-      const response = await dbx.filesListFolder({
-        path: coercePath(path),
-      })
+      const response = await dbx.filesListFolder({ path: coercePath(path) })
 
       const dirs: Types.List = response.result.entries
         .filter(isDirectory)
@@ -55,5 +91,5 @@ export function create({ token: accessToken }: Options): Types.Listable {
     }
   }
 
-  return { list }
+  return { list, read, write, createReadStream, writeStream }
 }
